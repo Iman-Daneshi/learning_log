81dd2cfcd63c1ab59329b26be44873b34e8de6a0
iman-daneshi <iman_daneshi@yahoo.com>
2022-05-23 10:59:56 +0430

    styling pages using django-bootstrap

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -34,7 +35,9 @@ INSTALLED_APPS = [
     # My apps
     'learning_logs',
     'users',
-
+    
+    # Third party apps.
+    'bootstrap4',
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
@@ -129,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # My settings
 LOGIN_URL = 'users:login'
+
+# Heroku settings.
+django_heroku.settings(locals())
